26ddd411c1727847e36d133d9e7370a83e664d73
dysphoniq <crenfrew14@gmail.com>
2018-03-04 00:55:22 -0600

    Display current info on home page

--- web/src/ActivityCounter/ActivityCounter.js
+++ web/src/ActivityCounter/ActivityCounter.js
@@ -8,7 +8,20 @@ class ActivityCounter extends Component {
 
 
   displayData(data) {
-    return data;
+    // var info = [];
+    // for (var key in data) {
+    //   info.push(<p key={key}>{key} : {data[key]}</p>);
+    // }
+    // return (
+    //   <div className="data-item" >{info}</div>
+    // );
+    var info = Object.entries(data.today).map(([type, count], index)=>{
+      return (
+        <div key={index}>{type} : {count} </div>
+      );
+    });
+
+    return info;
   }
 
   render() {
@@ -16,9 +29,10 @@ class ActivityCounter extends Component {
     if (this.props.data){
 
       var displayData = this.displayData(this.props.data);
+
       return (
         <div className="activity-counter">
-          {this.props.data.today.CR}
+          {displayData}
         </div>
       );
     }
